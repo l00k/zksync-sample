e7ebc389cb415e6e54b50dc06beeca2fc70b9afe
l00k <l00k+github@protonmail.com>
2022-03-02 15:05:15 +0100

    ux implementation

--- ui/src/etc/services.ts
+++ ui/src/etc/services.ts
@@ -1,16 +1,1 @@
-import { ToastProgrammatic as Toast } from 'buefy';
-import Clipboard from 'clipboard';
-
-export default {
-    clipboard: () => {
-        const clipboard = new Clipboard('.js-clipboard');
-
-        clipboard.on('success', function(e) {
-            Toast.open({
-                message: 'Copied!',
-                type: 'is-success',
-                position: 'is-bottom-right',
-            });
-        });
-    }
-};
+export default {};
